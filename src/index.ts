48c950236dc99949d2cc0080de21275e4c656d7b
chand1012 <chandler@multisiglabs.org>
2024-06-22 10:36:22 -0400

    Add redirect from KV system

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,17 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		// get the domain of the request
+		// eg example.com, www.example.com, sub.example.com
+		const domain = new URL(request.url).hostname;
+		// check if the domain is in the KV namespace
+		const rules = env.RULES;
+		const rule = await rules.get(domain);
+		// if the rule exists, permanently redirect to the new domain
+		if (rule) {
+			return Response.redirect(rule, 301);
+		}
+		// otherwise, return a 404
+		return new Response('Not Found', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
